138ece699fcbe559eefaf662d73ee754892d6795
william.mcmonagle <william.mcmonagle@arcadia.com>
2020-06-10 22:52:25 -0400

    Allow traffic from within VPC to RDS security group

--- ops/cf/vpc/sg_db.rb
+++ ops/cf/vpc/sg_db.rb
@@ -5,6 +5,7 @@ resource :SgDb, 'AWS::EC2::SecurityGroup', DependsOn: :Vpc do
   security_group_ingress [
     { CidrIp: '100.36.185.63/32', IpProtocol: :tcp, FromPort: 5432, ToPort: 5432 }, # ric
     { CidrIp: '108.31.47.47/32',  IpProtocol: :tcp, FromPort: 5432, ToPort: 5432 }, # billy
+    { CidrIp: Fn::get_att(:Vpc, :CidrBlock), IpProtocol: '-1', FromPort: 5432, ToPort: 5432 },
   ]
   security_group_egress [
     { CidrIp: '0.0.0.0/0', IpProtocol: '-1', FromPort: 0, ToPort: 0 }
